fd3765053990e231b82d5e4991d51a2c5c74fd4c
James Parkin <jamesaparkin2003@gmail.com>
2026-08-09 07:00:48 +0100

    Adjusted output formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import time
-
+import datetime
 
 def main() -> None:
 
@@ -23,24 +23,31 @@ def main() -> None:
             time_elapsed = (end - start)/1_000_000
 
             if response.status_code == 200:
-                print(f"{url} is online! ({time_elapsed}ms)")
+                # print(f"{url} is online! ({time_elapsed}ms)")
+                status = "UP"
             elif response.status_code == 403:
-                print(f"{url} is online, but you are forbidden! ({time_elapsed}ms)")
+                # print(f"{url} is online, but you are forbidden! ({time_elapsed}ms)")
+                status = "UP"
             elif response.status_code == 429:
-                print(f"{url} is online! However, too many requests are being sent ({time_elapsed}ms)")
+                # print(f"{url} is online! However, too many requests are being sent ({time_elapsed}ms)")
+                status = "UP"
             elif 400 <= response.status_code < 500:
-                print(f"{url} experienced a client error. ({time_elapsed}ms)")
+                # print(f"{url} experienced a client error. ({time_elapsed}ms)")
+                status = "UP"
             elif 500 <= response.status_code < 600:
-                print(f"{url} experienced a server error. ({time_elapsed}ms)")
+                # print(f"{url} experienced a server error. ({time_elapsed}ms)")
+                status = "UP"
             else:
-                print(f"{url} may be offline. ({time_elapsed}ms)")
+                # print(f"{url} may be offline. ({time_elapsed}ms)")
+                status = "DOWN"
+
+            print(f"{datetime.datetime.now().strftime('%H:%M:%S')} | {url} | {status} | {time_elapsed:.2f}ms")
 
         except requests.ConnectionError:
-            print(f"{url} Connection error")
+            print(f"{datetime.datetime.now().strftime('%H:%M:%S')} | {url} | DOWN | CONNECTION ERROR")
 
         except requests.Timeout:
-            print(f"{url} Timeout error")
-
+            print(f"{datetime.datetime.now().strftime('%H:%M:%S')} | {url} | DOWN | TIMEOUT")
 
 if __name__ == '__main__':
     main()
